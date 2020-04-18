323bab663ed1adfe1f27eca765c569b2000fa131
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-04-18 15:09:16 +0200

    groupDao findAll

--- com/chrosciu/dao/GroupDao.java
+++ com/chrosciu/dao/GroupDao.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 
 public class GroupDao {
     private static final String CREATE_GROUP =
@@ -52,5 +53,50 @@ public class GroupDao {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void update(Group group) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_GROUP);
+            statement.setString(1, group.getName());
+            statement.setInt(2, group.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int groupId) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_GROUP);
+            statement.setInt(1, groupId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Group[] findAll() {
+        try (Connection conn = DBUtil.connect()) {
+            Group[] groups = new Group[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_GROUPS);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Group group = new Group();
+                group.setId(resultSet.getInt("id"));
+                group.setName(resultSet.getString("name"));
+                groups = addToArray(group, groups);
+            }
+            return groups;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Group[] addToArray(Group g, Group[] groups) {
+        Group[] tmpGroups = Arrays.copyOf(groups, groups.length + 1);
+        tmpGroups[groups.length] = g;
+        return tmpGroups;
     }
 }
