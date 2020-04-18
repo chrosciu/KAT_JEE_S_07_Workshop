a4eb682711a4c3b65dfcd141d2504e5a1ae882a7
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-04-18 14:48:20 +0200

    groupDao

--- com/chrosciu/dao/GroupDao.java
+++ com/chrosciu/dao/GroupDao.java
@@ -3,10 +3,10 @@ package com.chrosciu.dao;
 import com.chrosciu.model.Group;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GroupDao {
     private static final String CREATE_GROUP =
@@ -19,6 +19,23 @@ public class GroupDao {
             "DELETE FROM groups WHERE id = ?";
     private static final String FIND_ALL_GROUPS =
             "SELECT * FROM groups";
+
+    public Group create(Group group) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_GROUP, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, group.getName());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                group.setId(resultSet.getInt(1));
+            }
+            return group;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public Group readGroup(int groupId) {
         try (Connection conn = DBUtil.connect()) {
